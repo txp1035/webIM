9ddb061428b64a9ddf0367cbe0a8ca3904cec3b8
DaoYuan <txp1035@gmail.com>
2018-12-26 17:13:00 +0800

    整理

---
+++ vue/app/component/Index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Index extends React.Component {
+  render() {
+    return <div>这是首页</div>;
+  }
+}
+
+export default Index;
--- vue/config/webpack/webpack.file.conf.js
+++ vue/config/webpack/webpack.file.conf.js
@@ -1,5 +1,5 @@
 module.exports = {
-    devDirectory: 'devBuild',/*开发目录*/
-    proDirectory: 'dist',/*发布目录*/
-    resource: 'resource',/*静态资源*/
+  devDirectory: 'devBuild' /*开发目录*/,
+  proDirectory: 'dist' /*发布目录*/,
+  resource: 'resource' /*静态资源*/
 };
