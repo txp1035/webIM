6611a202b7f5ccefb263e31d3168bec6ca1c3754
DaoYuan <txp1035@gmail.com>
2018-12-28 21:29:20 +0800

    移植

--- create-react-app/src/index.js
+++ create-react-app/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './style/style.less';
+import './style/iconfont/iconfont.css';
+import Router from './Router.js';
+// import Main from './component/Main.js';
+// import { HashRouter, Route, NavLink } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Router />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
