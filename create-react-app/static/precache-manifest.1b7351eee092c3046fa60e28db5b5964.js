d1824a3aa5b0fa3181c2b06c236cc25f07685553
DaoYuan <txp1035@gmail.com>
2019-01-03 17:39:45 +0800

    静态网站

---
+++ create-react-app/static/precache-manifest.1b7351eee092c3046fa60e28db5b5964.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "c782921affbe5fc3c2e3e929b94d1250",
+    "url": "/static/media/iconfont.c782921a.eot"
+  },
+  {
+    "revision": "8dd6d058cbec281819dc5ff5247a5290",
+    "url": "/static/media/iconfont.8dd6d058.svg"
+  },
+  {
+    "revision": "1466e36d20bd4cc5af7e67fa83dc9bee",
+    "url": "/static/media/iconfont.1466e36d.ttf"
+  },
+  {
+    "revision": "5a9adad6587422d20a82a0dbe865ac27",
+    "url": "/static/media/bg.5a9adad6.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5676e661f568ab676085",
+    "url": "/static/js/main.5676e661.chunk.js"
+  },
+  {
+    "revision": "6c7cd6d4e071d28f5fa1",
+    "url": "/static/js/1.6c7cd6d4.chunk.js"
+  },
+  {
+    "revision": "5676e661f568ab676085",
+    "url": "/static/css/main.c1acfa65.chunk.css"
+  },
+  {
+    "revision": "f19d98d18c4a3aaaf92875bf5dd21c28",
+    "url": "/index.html"
+  }
+];
